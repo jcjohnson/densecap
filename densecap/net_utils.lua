d3cefdafe7c9c3de073e6df0e864e47ba1ac1da4
Justin Johnson <j26774@gmail.com>
2016-04-21 07:47:23 -0700

    remove cutorch from net utils

--- densecap/net_utils.lua
+++ densecap/net_utils.lua
@@ -1,5 +1,4 @@
 require 'loadcaffe'
-require 'cutorch'
 
 
 local net_utils = {}
@@ -69,6 +68,9 @@ cudnn_algos['vgg-16']['GeForce GTX TITAN Z'] = {
 
 
 function net_utils.cudnn_tune_cnn(cnn_name, cnn)
+  if not cutorch then
+    return cnn
+  end
   local device = cutorch.getDevice()
   local device_name = cutorch.getDeviceProperties(device).name
   if cudnn_algos[cnn_name] and cudnn_algos[cnn_name][device_name] then
