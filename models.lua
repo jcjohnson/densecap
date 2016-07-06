98155aba979810bf5fece141b34b4a3b883b2f4a
Ruotian Luo <rluo@ttic.edu>
2016-07-06 12:09:13 -0500

    Fix some small bugs: 1. Wrong variable names when reloading the model 2. called_forward and called_backward to _called_forward_size and _called_backward_size

--- models.lua
+++ models.lua
@@ -8,15 +8,12 @@ function M.setup(opt)
   else
     print('initializing a DenseCap model from ' .. opt.checkpoint_start_from)
     model = torch.load(opt.checkpoint_start_from).model
-    model.opt.objectness_weight = opt.objectness_weight
-    model.nets.localization_layer.opt.obj_weight = opt.objectness_weight
-    model.opt.box_reg_weight = opt.box_reg_weight
+    model.opt.end_objectness_weight = opt.end_objectness_weight
+    model.nets.localization_layer.opt.mid_objectness_weight = opt.mid_objectness_weight
+    model.nets.localization_layer.opt.mid_box_reg_weight = opt.mid_box_reg_weight
     model.crits.box_reg_crit.w = opt.end_box_reg_weight
-    model.opt.classification_weight = opt.classification_weight
     local rpn = model.nets.localization_layer.nets.rpn
     rpn:findModules('nn.RegularizeLayer')[1].w = opt.box_reg_decay
-    model.opt.sampler_high_thresh = opt.iou_high_thresh
-    model.opt.sampler_low_thresh = opt.iou_low_thresh
     model.opt.train_remove_outbounds_boxes = opt.train_remove_outbounds_boxes
     model.opt.captioning_weight = opt.captioning_weight
 
