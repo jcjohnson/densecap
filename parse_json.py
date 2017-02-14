28a11cf8da3783154f2d51bc6991bf2b48865ee5
Xinpeng.Chen <2015202130029@whu.edu.cn>
2017-02-14 14:18:51 +0800

    Create parse_json.py

---
+++ parse_json.py
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+
+import os
+import json
+import cPickle
+
+paragraph_json_file = open('paragraphs_v1.json').read()
+paragraph = json.loads(paragraph_json_file)
+
+img2paragraph = {}
+
+for each_img in paragraph:
+    image_id = each_img['image_id']
+    each_paragraph = each_img['paragraph']
+    sentences = each_paragraph.split('. ')
+    if '' in sentences:
+        sentences.remove('')
+    img2paragraph[image_id] = [len(sentences), sentences]
+
+for key, para in img2paragraph.iteritems():
+    print key, para
+
+with open('img2paragraph', 'wb') as f:
+    cPickle.dump(img2paragraph, f)
